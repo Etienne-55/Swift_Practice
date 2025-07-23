ee60049a26636579e783d80faadc8e2fc874060c
Etienne <etienne.pautet@gmail.com>
2025-07-23 17:13:51 -0300

    Second commit

--- terminal_tldr.swift
+++ terminal_tldr.swift
@@ -1,24 +1,4 @@
-var condition = 0
-var secondCondition: Float = 0
 var thirdCondition: Int = 0
-
-repeat {
-    print("Enter a number:", terminator: " ")
-    if let input = readLine(), let number = Float(input) {
-        print("Looooop")
-        secondCondition = number
-    }
-} while secondCondition <= 420
-
-let someCharacter: Character = "a"
-switch someCharacter {
-case "a":
-    print("The first letter")
-case "z":
-    print("The last letter")
-default:
-    print("Some other character")
-}
 
 
 repeat {
@@ -26,7 +6,8 @@ repeat {
     print("-------Nvim-------")
     print("1->Deleting commands")
     print("2->Navigation commands")
-    print("3->Terminate")
+    print("3->Test function")
+    print("4->Terminate")
     print(":", terminator: " ")
     if let userInput = readLine(), let option = Int(userInput) {
     thirdCondition = option
@@ -36,8 +17,19 @@ repeat {
         print("In development")
     case 2:
         print("In development")
+    case 3:
+        test_function()
     default:
         print("Invalid option!!")
         }
     }
-} while thirdCondition != 3
+} while thirdCondition != 4
+
+
+func test_function() {
+    print("it works")
+}
+
+func navigationNvim() {
+    
+}
